6ecf919833c47b52e0f574bcd56a63e4d175df58
Rashmi Sinha <Rashmi.Sinha@ibm.com>
2021-04-08 11:08:43 +0530

    cange

--- Airline/Controllers/HomeController.cs
+++ Airline/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Airline.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.is ready";
 
             return View();
         }
